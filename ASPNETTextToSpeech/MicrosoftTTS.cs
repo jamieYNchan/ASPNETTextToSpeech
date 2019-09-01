9ec51bb9ed8a19c428570481f6523a5afbdb4bc4
jamie chan <jamie.yn.chan@gmail.com>
2019-09-01 23:28:46 +0800

    Change project name. Add Microsoft Speech to Text

---
+++ ASPNETTextToSpeech/MicrosoftTTS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNETTextToSpeech
+{
+    public class MicrosoftTTS
+    {
+        public int Volume { get => synthesizer.Volume; set => synthesizer.Volume = value; }
+        public int Rate { get => synthesizer.Rate; set => synthesizer.Rate = value; }
+        static SpeechSynthesizer synthesizer;
+        public MicrosoftTTS()
+        {
+            synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = -2;     // -10...10
+        }
+
+        public void TextToSpeech(string text)
+        {
+            synthesizer.SpeakAsync(text);
+        }
+    }
+}
---
+++ ASPNETTextToSpeech/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNETTextToSpeech
+{
+    class Program
+    {
+        static MicrosoftTTS _microsoftTTS;
+        static void Main(string[] args)
+        {
+            _microsoftTTS = new MicrosoftTTS();
+            readText();
+        }
+
+        static void readText()
+        {
+            Console.WriteLine("Type some text that you want to speak...");
+            Console.Write("> ");
+            string text = Console.ReadLine();
+            _microsoftTTS.TextToSpeech(text);
+            readText();
+        }
+    }
+}
